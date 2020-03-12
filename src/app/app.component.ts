b089a18b0aa8ac9c94c071e3b032fdf6ce2c2454
tabatagloria <tabatabruna@yahoo.com.br>
2020-03-12 18:33:10 -0300

    [ProjetoIntegradorADSFatec#1 Template Corrigido]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import Map from 'ol/Map.js';
+import View from 'ol/View.js';
+import OSM from 'ol/source/OSM';
+import TileWMS from 'ol/source/TileWMS';
+import TileLayer from   'ol/layer/Tile.js';
+import { defaults as defaultControls } from 'ol/control.js';
 
 @Component({ 
   selector: 'app-root',
@@ -9,15 +15,37 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 export class AppComponent {
   title = 'web-gis';
 
+
   constructor(private modalService: NgbModal){}
 
   openBackDropCustomClass(content) {
     this.modalService.open(content, { 
       centered: false,
       backdrop:false,
-      
       });
-  }
+      }
+
+      // this.osm = new TileLayer({
+      //   preload: Infinity,
+      //   visible: true,
+      //   title: "osm",
+      //   baseLayer: true,
+      //   source: new OSM(),
+      //   layer:  osm,
+      // });
+
+      // var view = new View({
+      //   center: [-6124801.2015023, -1780692.0106836],
+      //   zoom: 4
+      // });
+      
+      // this.map = new Map({
+      //       controls: defaultControls(),
+      //       layers: this.osm,
+      //       target: 'map',
+      //       view: view
+      // });
+     
 
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
 import { FooterComponent } from './footer/footer.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -14,6 +17,10 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    InputSwitchModule,
+    DropdownModule,
+    FormsModule,
+    BrowserAnimationsModule,
     NgbModule
   ],
   providers: [],
