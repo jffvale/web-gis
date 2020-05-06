46dffa20ee3d458d89496891a461a37a6538bd1c
AbnerErnaniADSFatec <abiner.anjos@gmail.com>
2020-05-06 10:48:32 -0300

    [#16] Configuração inicial para as buscas na API

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,5 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import './polyfills';
-
-import {HttpClientModule} from '@angular/common/http';
-import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatNativeDateModule} from '@angular/material';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {DemoMaterialModule} from './material-module';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
